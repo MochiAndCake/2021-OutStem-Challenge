d0b611e45479c5c705bdc5dcfa66c465e53f175d
Ann JS <36861363+MochiAndCake@users.noreply.github.com>
2021-05-23 14:44:48 -0400

    Added validation

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
+//import { ObjectUnsubscribedError } from 'rxjs';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+  form: FormGroup;
+  submitAttempt: Boolean;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.submitAttempt = false;
+    this.form = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      checkbox: [null, Validators.required]
+    })
+  }
+
+  isFieldInvalid(fld: string) {
+    return !this.form.get(fld).valid && this.submitAttempt;
+    //(!this.form.get(fld).valid && this.form.get(fld).touched) || (this.form.get(fld).untouched && this.submitAttempt);
+  }
+
+  showError(fld: string) {
+    return{
+      'error': this.isFieldInvalid(fld)
+    };
+  }
+
+  onSubmit() {
+    this.submitAttempt = true;
+    if(this.form.valid){
+      this.submitAttempt = false;
+      this.form.reset();
+      // If form invalid, loop through each field
+      // Retrieve control object as we iterate.
+      // Mark control as touched to trigger validation.
+      // Learned from: https://loiane.com/2017/08/angular-reactive-forms-trigger-validation-on-submit/#displaying-the-validation-error-message
+      // Object.keys(this.form.controls).forEach(fld => {
+      //   const control = this.form.get(fld);
+      //   control.markAsTouched({ onlySelf: true});
+      // });
+    }
+  }
+
+  //"/^[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+}
